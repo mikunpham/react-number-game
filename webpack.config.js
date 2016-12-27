1306d28b97c4b20801a23cecc9177443e3236901
mikunpham <mikun1993@gmail.com>
2016-12-27 17:54:36 +0700

    Add Webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack')
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: './public/js',
+		filename: 'app.bundle.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, exclude: /node_modules/ ,loader: 'babel-loader' }
+		]
+	},
+	plugins: [
+		// new webpack.optimize.UglifyJsPlugin({
+		// 	compress: {
+		// 		warnings: false
+		// 	},
+		// 	output: {
+		// 		comments: false
+		// 	}
+		// }),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		})
+	]
+}
